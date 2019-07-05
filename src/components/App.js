402024a2a54e38b47e626b4cdafb85c696811546
THOMAS Pierre-alexandre <contact@referia.com>
2019-07-05 19:51:31 +0200

    Installation files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Shopping list</h3>
+                <div>Under construction</div>
+            </div>
+        );
+    }
+}
+
+export default App;
