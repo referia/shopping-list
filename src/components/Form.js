40a84212856191213ab2a0ba876eb168881fbf48
THOMAS Pierre-alexandre <contact@referia.com>
2019-07-06 12:20:46 +0200

    Class based component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
-const Form = () => {
-    return (
-        <div>Form component</div>
-    );
-};
+class Form extends React.Component {
+    state = {
+        name: '',
+        quantity: 0
+    };
+
+    render() {
+        return (
+            <div>
+                <h3>Ajouter des articles à acheter</h3>
+                <form>
+                <input 
+                    type="number" 
+                    value={this.state.quantity} 
+                    onChange={(event) => this.setState({ quantity: event.target.value})} 
+                    placeholder="quantity" />
+                <input 
+                    type="text" 
+                    value={this.state.name} 
+                    onChange={(event) => this.setState({ name: event.target.value})} 
+                    placeholder="article" />
+                <button type="submit">ajouter</button>
+                </form>
+            </div>
+        );
+    }
+}
+
 
 export default Form;
